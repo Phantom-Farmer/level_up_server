2df041d1dfa35a77be32714d5c9a157f25c7dc64
Phantom-Farmer <joeljdahl@gmail.com>
2022-11-22 17:47:00 -0600

    first commit

---
+++ levelupapi/models/event.py
@@ -0,0 +1,14 @@
+from django.db import models
+from .gamer import Gamer
+from .game import Game
+
+class Event(models.Model):
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    description = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+    organizer = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
---
+++ levelupapi/models/event_gamer.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .gamer import Gamer
+from .event import Event
+
+
+
+class EventGamer(models.Model):
+    gamer = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
