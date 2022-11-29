d0277953a8b4ed39f13127a9a89aa7b39b1a3438
Phantom-Farmer <joeljdahl@gmail.com>
2022-11-28 20:05:23 -0600

    added event list and retrieve

--- levelup/urls.py
+++ levelup/urls.py
@@ -18,11 +18,12 @@ from django.urls import path
 from levelupapi.views import register_user, check_user
 from django.conf.urls import include
 from rest_framework import routers
-from levelupapi.views import GameTypeView, GameView
+from levelupapi.views import GameTypeView, GameView, EventView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype',)
 router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('register', register_user),
