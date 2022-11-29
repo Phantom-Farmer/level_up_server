240f27f1376fe8ed94b689ac120da3b85886b776
Phantom-Farmer <joeljdahl@gmail.com>
2022-11-28 19:15:38 -0600

    added game list and retrieve

--- levelup/urls.py
+++ levelup/urls.py
@@ -18,10 +18,11 @@ from django.urls import path
 from levelupapi.views import register_user, check_user
 from django.conf.urls import include
 from rest_framework import routers
-from levelupapi.views import GameTypeView
+from levelupapi.views import GameTypeView, GameView
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'gametypes', GameTypeView, 'gametype',)
+router.register(r'games', GameView, 'game')
 
 urlpatterns = [
     path('register', register_user),
