0bc98a571f382d82465f358d2d0b22597ed499d6
Phantom-Farmer <joeljdahl@gmail.com>
2022-11-29 18:14:38 -0600

    chapter 8 start

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -3,32 +3,29 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Event
+from levelupapi.models import Event, Gamer, Game
 
 
 class EventView(ViewSet):
-    """Level up game types view"""
-
     def retrieve(self, request, pk):
         """Handle GET requests for single event
-
-        Returns:
-            Response -- JSON serialized event
         """
-        """try:"""
-        event = Event.objects.get(pk=pk)
-        serializer = EventSerializer(event)
-        return Response(serializer.data)
-
-
-    def list(self, request):
-        """Handle GET requests to get all events
-
-        Returns:
-            Response -- JSON serialized list of events
-        """
+        try:
+            event = Event.objects.get(pk=pk)
+            serializer = EventSerializer(event)
+            return Response(serializer.data)
+        except Event.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
         
+    def list(self, request):
+        """Handle GET requests for all events
+        """
         events = Event.objects.all()
+        
+        game = request.query_params.get('game', None)
+        if game is not None:
+            events = events.filter(game_id=game)
+        
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
       
@@ -38,3 +35,4 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ('id', 'game', 'description', 'date', 'time', 'organizer')
+        depth = 1
