de175906b7a533fa5b5e7b99c73a4c95eb14259f
Phantom-Farmer <joeljdahl@gmail.com>
2022-12-07 21:37:31 -0600

    booya

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -9,6 +9,15 @@ class Event(models.Model):
     date = models.DateField(auto_now=True)
     time = models.TimeField(auto_now=True)
     organizer = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    objects = models.Manager()
 
     def __str__(self):
         return self.name
+    
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
